1f47976eec05ea3e05c3f7f24a6cd8b71d94547b
GCout <Gccoutret@gmail.com>
2019-10-03 12:52:16 -0500

    added vars for each api and began swtch statemnts

--- liri.js
+++ liri.js
@@ -1,4 +1,22 @@
 require("dotenv").config();
+
 var keys = require("./keys.js")
-var inquirer = require("inquirer");
+var fs = require('fs');
+var moment = require('moment')
+var axios = require("axios");
 var spotify = new Spotify(keys.spotify);
+
+function liriDoThis(action){
+  console.log("What can I help you find?");
+  switch(action){
+    case 'movie-this':
+
+      break;
+    case 'spotify-this-song':
+      break;
+    case '':
+      break;
+
+  }
+  
+}
